c5eb7923a904cd3a35278b1d1fe7b50664931ad0
Hyuntae Joo <hyuuuntae@gmail.com>
2017-12-22 20:19:00 +0900

    Convert indentation 4 spaces to 2 spaces in album.js

--- public/js/album.js
+++ public/js/album.js
@@ -1,57 +1,57 @@
 'use strict';
 
 angular.module('printemps').controller('albumController', function($scope, $http) {    
-    var selectedAlbum = null,
-        scope$ = angular.element($('#header-play')).scope();
+  var selectedAlbum = null,
+    scope$ = angular.element($('#header-play')).scope();
 
+  $scope.albums = [];
+
+  (function() {
+    listMusics();
+    $('.shape').shape();
+  }());
+
+  function getImage(format, data) {
+    var base64String = '';
+    for(let i=0, ni=data.length; i<ni; i+=1) { base64String += String.fromCharCode(data[i]); }
+    return `data:${format};base64,${window.btoa(base64String)}`;
+  }
+
+  function listMusics() {
     $scope.albums = [];
+    $http.post('/', null).then(res => {
+      //console.log('albumCount:', res.data.length);
+      res.data.forEach((e, i) => {
+        $scope.albums[i] = {
+          artist: e.artist,
+          title: e.title,
+          year: e.musics[0].year.substr(0, 4),//@@
+          image: getImage(e.format, e.image),
+          musics: e.musics.sort((m, _m) => { return m.track - _m.track; })
+        };
+      });
+    });
+  }
 
-    (function() {
-        listMusics();
-        $('.shape').shape();
-    }());
+  function flipShape(selected, behavior) {
+    var query = '[id="{0}@{1}"]'.format(selected[0], selected[1]);
+    $(query).shape(behavior);
+  }
 
-    function getImage(format, data) {
-        var base64String = '';
-        for(let i=0, ni=data.length; i<ni; i+=1) { base64String += String.fromCharCode(data[i]); }
-        return `data:${format};base64,${window.btoa(base64String)}`;
-    }
+  $scope.showMusics = function(artist, title) {
+    if(selectedAlbum !== null){ flipShape(selectedAlbum, 'flip back'); }
+    selectedAlbum = [artist, title];
+    flipShape(selectedAlbum, 'flip over');
+  };
 
-    function listMusics() {
-        $scope.albums = [];
-        $http.post('/', null).then(res => {
-            //console.log('albumCount:', res.data.length);
-            res.data.forEach((e, i) => {
-                $scope.albums[i] = {
-                    artist: e.artist,
-                    title: e.title,
-                    year: e.musics[0].year.substr(0, 4),//@@
-                    image: getImage(e.format, e.image),
-                    musics: e.musics.sort((m, _m) => { return m.track - _m.track; })
-                };
-            });
-        });
-    }
+  $scope.showAlbum = function(artist, title) {
+    flipShape(selectedAlbum, 'flip back');
+    selectedAlbum = null;
+  };
 
-    function flipShape(selected, behavior) {
-        var query = '[id="{0}@{1}"]'.format(selected[0], selected[1]);
-        $(query).shape(behavior);
-    }
-
-    $scope.showMusics = function(artist, title) {
-        if(selectedAlbum !== null){ flipShape(selectedAlbum, 'flip back'); }
-        selectedAlbum = [artist, title];
-        flipShape(selectedAlbum, 'flip over');
-    };
-
-    $scope.showAlbum = function(artist, title) {
-        flipShape(selectedAlbum, 'flip back');
-        selectedAlbum = null;
-    };
-
-    $scope.playMusic = function(album, music, index) {
-        // console.log(album.musics, index);
-        scope$.play(album, index);
-    };
+  $scope.playMusic = function(album, music, index) {
+    // console.log(album.musics, index);
+    scope$.play(album, index);
+  };
 
 });
