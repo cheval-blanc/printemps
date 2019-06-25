9df0cdf6f3b0e787289c438897d221ad6357bb0d
Hyuntae Joo <hyuuuntae@gmail.com>
2019-06-25 13:35:19 +0900

    refactor(ui): init ui module and remove legacy codes
    modify index.html only leave linking bundle.js
    dom will be appended dynamically by main.js and App.vue

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
-'use strict';
+import Vue from 'vue';
 
-import Vue from 'vue/dist/vue.esm.js';
-import 'semantic-ui-css/semantic.min.css';
-
-import './scss/header.scss';
-import './scss/main.scss';
-
-import { volumeCtrl } from './volume';
-import { playCtrl } from './play';
-import { albumListCtrl } from './albumList';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-export let vueApp = new Vue({
+new Vue({
   el: '#app',
-  data: {
-    audioCtx: new Audio(),
-  },
-  mixins: [volumeCtrl, playCtrl, albumListCtrl],
+  render: h => h(App)
 });
