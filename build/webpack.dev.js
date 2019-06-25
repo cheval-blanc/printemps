5f685d70a8bdbcc0eb79c9462b1bee2332a7ad03
Hyuntae Joo <hyuuuntae@gmail.com>
2019-06-25 13:25:11 +0900

    refactor(common): modify npm scripts and move webpack config files
    modify npm script to use nodemon for running server
    update npm plugins
    apply babel and add babel config file
    
    move webpack config files to ./build and apply to the npm scripts

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -3,10 +3,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
+const { PROD_PATH } = require('./path');
+
 module.exports = merge(common, {
+  mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: PROD_PATH,
     proxy: {
       '**': 'http://localhost:3000'
     },
