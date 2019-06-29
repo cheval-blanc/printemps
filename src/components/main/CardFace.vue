a300c371927cf669fa5772dafd566a71175d5292
Hyuntae Joo <hyuuuntae@gmail.com>
2019-06-29 18:30:21 +0900

    feat(ui): add album-desc in CardFace
    define text-ellipsis mixin to deal with overflowing texts
    album-desc will shrink depends on height of AlbumCard

--- src/components/main/CardFace.vue
+++ src/components/main/CardFace.vue
@@ -2,12 +2,21 @@
   <div class="card-face">
     <img class="album-art" src="https://via.placeholder.com/400" />
 
+    <div class="album-desc">
+      <p class="album-title">{{ albumTitle }}</p>
+      <p class="pub-year">{{ pubYear }}</p>
+      <p class="artist">{{ artist }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: ()=>({
+    albumTitle: 'Lorem ipsum dolor sit amet',
+    pubYear: '2019',
+    artist: 'Lorem ipsum',
+  }),
 }
 </script>
 
@@ -19,10 +28,41 @@ export default {
   @include card-style();
   transform: rotateY(0deg);
   z-index: 1;
+  display: flex;
+  flex-direction: column;
 
   .album-art {
+    flex: none;
     width: 100%;
     border-radius: $card-border-radius $card-border-radius 0 0;
+  }
+
+  .album-desc {
+    flex: 1;
+    position: relative;
+    margin: 4px $md-pad 6px $md-pad;
+
+    & > p {
+      @include text-ellipsis();
+      margin: 0;
+      color: $bold-color;
+    }
+
+    .album-title {
+      margin-bottom: 3px;
+      font-size: 1.0em;
+      font-weight: 600;
+    }
+    .pub-year {
+      font-size: 0.8em;
+      color: $primary-color;
+    }
+    .artist {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      font-size: 0.9em;
+    }
   }
 }
 </style>
