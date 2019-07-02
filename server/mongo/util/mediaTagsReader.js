c6cd208647155df42dad1825f8f97586d51d85f6
Hyuntae Joo <hyuuuntae@gmail.com>
2019-07-02 16:17:37 +0900

    feat(mongo): add mediaTagsReader using jsmediatags
    downgrade version of jsmediatags ^3.8.2 to ^3.8.0
    because of an error that cannot find module 'jsmediatags' when running node.js
    
    implement function that returns Promise to use async/await with jsmediatags
    ref: https://stackoverflow.com/questions/49320073/using-await-with-jsmediatags

---
+++ server/mongo/util/mediaTagsReader.js
@@ -0,0 +1,25 @@
+import jsmediatags from 'jsmediatags';
+
+const tagsToRead = [
+  'title',
+  'artist',
+  'album',
+  'year',
+  'track',
+  'picture',
+];
+
+export default function(fileName) {
+  return new Promise((resolve, reject) => {
+    new jsmediatags.Reader(fileName)
+      .setTagsToRead(tagsToRead)
+      .read({
+        onSuccess(data) {
+          resolve(data.tags);
+        },
+        onError(error) {
+          reject(error);
+        },
+      });
+  });
+}
