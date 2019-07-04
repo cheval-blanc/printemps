c06775b1ed8ad187befd5b1fdb5d45166351670a
Hyuntae Joo <hyuuuntae@gmail.com>
2019-07-04 15:33:19 +0900

    feat(server): add '/albums' api to request all albums from mongo

--- server/router.js
+++ server/router.js
@@ -1,7 +1,14 @@
 import path from 'path';
 
+import { listAlbums } from './mongo/controllers/albumController';
+
 export default function(app) {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../index.html'));
+  });
+
+  app.get('/albums', async (reg, res) => {
+    const albums = await listAlbums();
+    res.json(albums);
   });
 }
