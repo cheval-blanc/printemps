fc58066366c062e2089b754236a529cc5c3b1c49
Hyuntae Joo <hyuuuntae@gmail.com>
2019-08-14 10:26:05 +0900

    feat(common): apply eslint and prettier
    add config files for eslint, prettier, and editor
    add npm script for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'prettier',
+    'prettier/standard',
+  ],
+  plugins: [
+    'vue',
+    'prettier',
+  ],
+  rules: {
+    'generator-start-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error',
+  },
+};
